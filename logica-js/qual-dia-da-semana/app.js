0c61fdde270f2a8d0733bc14eef7151054455014
Tiago Martins <tiagoalmeidasp@hotmail.com>
2026-02-20 08:00:39 -0300

    feat: executando desafios propostos

---
+++ logica-js/qual-dia-da-semana/app.js
@@ -0,0 +1,45 @@
+/*let diaDaSemana = prompt('Que dia da semana é hoje?');
+
+if(diaDaSemana === 'domingo' || diaDaSemana === 'sábado') {
+    alert('Hoje é dia de descanso! Bom final de semana!');
+} else {
+    alert('Hoje é dia de trabalho! Tenha um ótimo dia!');
+}*/
+
+
+/*
+// Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+let numero = prompt('Digite um número qualquer.');
+
+if(numero > 0) {
+    alert('O número digitado é positivo!');
+} else if(numero < 0) {
+    alert('O número digitado é negativo!');
+} else {
+    alert('O número digitado é zero!');
+}*/
+
+//Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+/*let pontuacao = prompt('Digite sua pontuação no jogo.');
+
+if(pontuacao >= 100) {
+    alert('Parabéns, você venceu!');
+} else {
+    alert('Tente novamente para ganhar.');
+}*/
+
+/*// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+let saldo = prompt('Digite o valor do seu saldo bancário.');
+alert(`O saldo da sua conta é R$ ${saldo}.`);*/
+
+/*// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+let nome = prompt('Digite seu nome.');
+
+// se a última letra do nome for "a", mostre "Bem-vinda, [nome]!", caso contrário, mostre "Bem-vindo, [nome]!".
+if(nome.endsWith('a')) {
+    alert(`Bem-vinda, ${nome}!`);
+} else {
+    alert(`Bem-vindo, ${nome}!`);
+}*/
+
+
